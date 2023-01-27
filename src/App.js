3636eb12c98ac914208f08120edd5907b5989858
Ajay-at-005173903299 <anurag.pandey17@gmail.com>
2023-01-27 16:44:55 +0530

    fianl

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import './App.css';
-import Faqcontent from './Components/Faqcontent';
-import { Footer } from './Components/Footer';
-import { Header } from './Components/Header';
-import { Homecotnent } from './Components/Homecotnent';
-import { ProductSort } from './Components/ProductSort';
-import { ProductSort1 } from './Components/Searchfilter';
-import MobHeader from './Components/MobHeader';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import Faqcontent from "./Components/Faqcontent";
+import { Footer } from "./Components/Footer";
+import { Header } from "./Components/Header";
+import { Homecotnent } from "./Components/Homecotnent";
+import { ProductSort } from "./Components/ProductSort";
+import { ProductSort1 } from "./Components/Searchfilter";
+import MobHeader from "./Components/MobHeader";
+import { Fragment, useEffect, useState } from "react";
 
 function App() {
   const [sorttypes, setSorttype] = useState();
   useEffect(() => {
-    setSorttype(JSON.parse(localStorage.getItem("sorttype")))
+    setSorttype(JSON.parse(localStorage.getItem("sorttype")));
   }, [localStorage]);
   console.log(sorttypes);
   return (
-    <div className="App">
-      
-      <Header/>
-      <MobHeader/>
-      <Homecotnent/>
-      {/* <ProductSort/> */}
-      <ProductSort1/>
-      <Faqcontent/>
-      <Footer/>
-     
-    </div>
+    <Fragment>
+      <div className="App">
+        <Header />
+        <MobHeader />
+        <Homecotnent />
+        {/* <ProductSort/> */}
+        <ProductSort1 />
+        <Faqcontent />
+        <Footer />
+      </div>
+    </Fragment>
   );
 }
 
